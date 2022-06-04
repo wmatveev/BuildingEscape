dc6dd3c430f00b877b737cd51a93e8a7655ca506
Vladimir Matveev <matveev@matller.com>
2022-06-04 10:54:13 +0300

    Added Pressure Plate

--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -24,5 +25,10 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenDoor = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume *PressurePlate;
 };
